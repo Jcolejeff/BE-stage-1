237de27741cb72e63acbb1846409195ba504a2e4
Jcolejeff <ikwuhjefferye@gmail.com>
2025-01-29 11:37:03 +0100

    updated readme

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-@app.get("/")
+@app.get("/hire/python-developers")
 def get_basic_info():
     """
     Returns basic information in JSON format:
